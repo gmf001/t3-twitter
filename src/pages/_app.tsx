f21d789b1e2347f71285525c18d60f8067ecba2f
Glenn Francis <xglennfrancisx@gmail.com>
2023-02-14 16:06:52 +1000

    hide devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/layout";
 import { api } from "@/utils/api";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "@/styles/globals.css";
 
@@ -17,7 +17,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <Component {...pageProps} />
       </Layout>
 
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </SessionProvider>
   );
 };
